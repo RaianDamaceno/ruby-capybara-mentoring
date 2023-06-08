4f4b2eef518461cd518cdc07b59038eb0448624d
Raian Damaceno <36999338+RaianDamaceno@users.noreply.github.com>
2023-06-08 20:57:38 -0300

    inital setup

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+# gem "rails"
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in ruby-capybara-mentoring.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/ruby/capybara/mentoring.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "mentoring/version"
+
+module Ruby
+  module Capybara
+    module Mentoring
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
